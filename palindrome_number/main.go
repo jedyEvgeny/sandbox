2ff75de580c28cc87d51ae5dc136874cb4b6bd3a
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-06-25 22:54:23 +1000

    Create main.go

---
+++ palindrome_number/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	if x < 10 {
+		return true
+	}
+	var reversed int
+	tempX := x
+	for tempX != 0 {
+		reversed = tempX%10 + reversed*10
+		tempX /= 10
+	}
+	return reversed == x
+}
+
+func main() {
+	fmt.Println("Поиск числапалиндрома")
+	fmt.Println("Палиндром - когда справа налево читается как слева направо")
+	fmt.Println("-------------")
+	fmt.Println("Введите число для проверки")
+	var num int
+	fmt.Scan(&num)
+	ok := isPalindrome(num)
+	fmt.Println(ok)
+}
