833571686b3780d6ddc2ff849e72c012d17599fa
Евгений Клюкман <Evgeniy.Klyukman@mail.ru>
2024-10-24 12:04:55 +1000

    Update main.go

--- Конфигурация с переменной оболочки/main.go
+++ Конфигурация с переменной оболочки/main.go
@@ -37,7 +37,7 @@ func main() {
 
 func (a app) run() error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Привет, Golang-инженеры!"))
 	})
